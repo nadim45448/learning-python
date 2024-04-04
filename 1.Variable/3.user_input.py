8073c31687b7e289c32d612cc91e6ec47ad3fb84
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2024-04-04 14:51:37 +0600

    take user input & type casting

---
+++ 1.Variable/3.user_input.py
@@ -0,0 +1,45 @@
+#input()
+#input('Tell me your age: ')
+
+
+ 
+name=input('Tell us your name:')
+age=input('Tell us your age:')
+#print(age)
+#print(name, age)
+#print(type(age))
+print(name,end=" ")
+print(age)
+
+
+"""
+num1=input("Enter first number:")
+num2=input("Enter second number:")
+sum=num1+num2
+#print(sum)
+#print(type(sum))
+num1_int=int(num1)
+num2_int=int(num2)
+result=num1_int+num2_int
+print(result)
+print(type(result))
+"""
+
+"""
+num1=input("first number:")
+num2=input("second number:")
+sum=int(num1)+int(num2)
+print(sum)
+"""
+
+"""
+num1=int(input("first number:"))
+num2=int(input("second number:"))
+result=num1+num2
+print(result)
+"""
+
+
+
+
+
