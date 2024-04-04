c4cc95ae7eacdc6c6f436629401aebc1b8a46fb2
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2024-04-04 11:59:02 +0600

    variable declaration & data type & f string

---
+++ 1.Variable/1.variable.py
@@ -0,0 +1,44 @@
+
+# 1. Intetger type variable
+"""  
+a=10
+b=8
+age=22
+price=100
+# print(age)
+# print(price)
+
+another_variable=price
+#print(another_variable)
+
+sum=age+price
+print(sum)
+"""
+# 2. String type variable
+"""
+name="Nadim Bin Hossain"
+# print(name)
+country='Bangladesh'
+# print(country)
+first_name='Nadim'
+last_name="Hossain"
+full_name=first_name+' '+last_name
+# print(full_name)
+
+hobby="To play football and \ncricket" #\n=new line
+print(hobby)
+"""
+
+
+# 3. Boolean type variable
+"""
+is_passed=True
+is_fail=False
+ """
+
+
+#concat string and int type variable
+course="Python"
+#print("Welcome to " +course +" learning")
+age=20
+print("He is ",age," years old")
---
+++ 1.Variable/2.data_type.py
@@ -0,0 +1,20 @@
+
+weight=65
+weight_type=type(weight)
+print(weight_type)
+
+price=50.35
+price_type=type(price)
+print(price_type)
+
+city="Dhaka"
+print(type(city))
+fail=False
+print(type(fail))
+
+#f string
+name= "X"
+age=15
+#about_me="My name is "+name +age
+about_me=f"My name is {name}. My age is {age}"
+print(about_me)
