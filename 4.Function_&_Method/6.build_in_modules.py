b7b7f879f86c9accf2cde57b5913de070c514833
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2024-04-09 10:19:01 +0600

    explore build in modules

---
+++ 4.Function_&_Method/6.build_in_modules.py
@@ -0,0 +1,18 @@
+# import math
+from math import sin, ceil
+import random
+# result=sin(30)
+result=ceil(2.25)
+# print(result)
+
+num =random.random()
+# print(num)
+
+a=random.randint(40,50)
+# print(a)
+
+winner=random.choice([4,8,6,2,25,65,88,5,10])
+# print(winner)
+
+import datetime as dt
+dt.date()
